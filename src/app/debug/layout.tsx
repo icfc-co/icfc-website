68b7b37441a9fba9b58c59a50c09c53337a7829c
mohdadnan-raza <mohdadnan.raza24@gmail.com>
2025-09-26 18:59:57 -0600

    Linting errors

--- src/app/debug/layout.tsx
+++ src/app/debug/layout.tsx
@@ -3,10 +3,6 @@ export const prerender = false;
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-export default function DebugLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DebugLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
